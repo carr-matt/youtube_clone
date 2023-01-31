660c8216e3a283bd45cc8f3cecd216d7ee92bd92
Matt <carr.matthew.lee@gmail.com>
2023-01-31 01:08:19 -0600

    VideoCard working, styled

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,9 @@
-import { Category } from "@mui/icons-material"
+// import { Category } from "@mui/icons-material"
 import { Stack } from "@mui/material"
 import { categories } from "../utils/constants"
 
-const selectedCategory = 'New'
 
-const Sidebar = () => (
+const Sidebar = ({selectedCategory, setSelectedCategory}) => (
     <Stack
         direction="row"
         sx={{
@@ -16,6 +15,7 @@ const Sidebar = () => (
             {categories.map((category) => (
                 <button
                     className="category-btn"
+                    onClick={() => setSelectedCategory(category.name)}
                     style={{
                         background: category.name === selectedCategory && '#FC1503', color: 'white'}}
                     key={category.name}>
